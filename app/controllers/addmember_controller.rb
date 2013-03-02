830fc880de78178876f008f325bc17ef6cfcef3f
Mike Land <mikepland@gmail.com>
2013-03-02 17:12:54 -0600

    Added ability to add new members

--- app/controllers/addmember_controller.rb
+++ app/controllers/addmember_controller.rb
@@ -1,13 +1,18 @@
 class AddmemberController < ApplicationController
 
 def new
-
+  session[:id] = Person.id
+  @person = Person.find(session[:person_id])
 end
 
 def create
   @person = Person.new(:email =>params[:email])
   AddmembersMailer.invite(@person).deliver
   redirect_to person_path(session[:person_id])
+end
+
+def confirm
+
 end
 
 
