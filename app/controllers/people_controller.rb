fb813eac4e7a98c017c61cdfd7cc1f92b07b8a44
Mike Land <mikepland@gmail.com>
2013-03-02 13:01:10 -0600

    changed redirect

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -62,7 +62,7 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       if @person.save
-        format.html { redirect_to @person, notice: 'Person was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Person was successfully created.' }
         format.json { render json: @person, status: :created, location: @person }
       else
         format.html { render action: "new" }
