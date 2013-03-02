9596f1b4f588c7a5a6f33bc79737fb6844a7036d
Mike Land <mikepland@gmail.com>
2013-03-01 21:19:12 -0600

    fix mass assignment

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  attr_accessible :age, :trail_name, :family_id, :gender, :role, :avatar_id, :email, :password_digest, :password_confirmation
+  attr_accessible :age, :trail_name, :family_id, :gender, :role, :avatar_id, :email, :password, :password_confirmation
 
   belongs_to :family
   belongs_to :avatar
