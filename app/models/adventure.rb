3272cca788357dd42e56921a49c95f5948359cd5
Kitty Singsuwan <kitty@kittysingsuwan.com>
2013-03-03 14:51:58 -0600

    fixed family can't be blank error on create new adventure

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,7 +1,7 @@
 class Adventure < ActiveRecord::Base
   attr_accessible :activity_id, :family_id, :name, :tips, :person_id
 
-  validates_presence_of :activity_id, :family_id, :name, :tips
+  validates_presence_of :activity_id, :name, :tips
 
   belongs_to :activity
   belongs_to :person
